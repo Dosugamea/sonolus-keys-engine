91ef2923211c7b07a2f0548cd641f353231d52c6
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2022-01-03 02:22:35 +0900

    🐛 Fix dev level didn't load local bgm

--- src/serve.ts
+++ src/serve.ts
@@ -16,17 +16,4 @@ import { buildOutput } from '.'
 
 const sonolus = serve(buildOutput)
 
-const level = sonolus.db.levels[0]
-
-level.bgm = sonolus.add('LevelBgm', './src/bgm.mp3')
-
-level.cover = {
-    type: 'LevelCover',
-    hash: '',
-    url: 'https://sonolus.com/assets/jacket066.png',
-}
-level.bgm = {
-    type: 'LevelBgm',
-    hash: '',
-    url: 'https://sonolus.com/assets/bgm066.mp3',
-}
+sonolus.db.levels[0].bgm = sonolus.add('LevelBgm', './src/bgm.mp3')
